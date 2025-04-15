a646a8c1e48f90c59db3e782875f16774e4c382d
Jaromirjjur <jaromirjjur@gmail.com>
2025-04-15 18:59:06 +0200

    Initial commit

---
+++ Sinner's lounge/drinks.js
@@ -0,0 +1,44 @@
+const drinks = [
+    {
+      name: "Martini",
+      ingredients: ["gin", "dry vermouth"],
+      image: "images/martini.jpg",
+      recipePage: 18
+    },
+    {
+      name: "Tom Collins",
+      ingredients: ["gin", "lemon juice", "sugar syrup", "sparkling water"],
+      image: "images/tom-collins.jpg",
+      recipePage: 19
+    },
+    {
+      name: "Belle Collins",
+      ingredients: ["gin", "lemon juice", "sugar syrup", "sparkling water"],
+      image: "images/belle-collins.jpg",
+      recipePage: 19
+    },
+    {
+      name: "Gin Rickey",
+      ingredients: ["gin", "lime juice", "sparkling water"],
+      image: "images/gin-rickey.jpg",
+      recipePage: 20
+    },
+    {
+      name: "Palm Beach",
+      ingredients: ["gin", "white rum", "pineapple juice"],
+      image: "images/palm-beach.jpg",
+      recipePage: 22
+    },
+    {
+      name: "Gin Sling",
+      ingredients: ["gin"],
+      image: "images/gin-sling.jpg",
+      recipePage: 23
+    },
+    {
+      name: "Maiden's Prayer",
+      ingredients: ["gin", "triple sec", "orange juice", "lemon juice"],
+      image: "images/maidens-prayer.jpg",
+      recipePage: 23
+    }
+  ];
