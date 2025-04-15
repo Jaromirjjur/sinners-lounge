0152637639453e22cb581844899569e7c46f0856
Jaromirjjur <jaromirjjur@gmail.com>
2025-04-15 19:50:34 +0200

    Update drinks.js

--- drinks.js
+++ drinks.js
@@ -40,5 +40,167 @@ const drinks = [
       ingredients: ["gin", "triple sec", "orange juice", "lemon juice"],
       image: "images/maidens-prayer.jpg",
       recipePage: 23
+    },
+    {
+      name: "Daisy",
+      ingredients: ["gin", "grenadine", "lemon juice", "sugar syrup", "sparkling water"],
+      image: "images/daisy.jpg",
+      recipePage: 24
+    },
+    {
+      name: "Hawaiian Orange Blossom",
+      ingredients: ["gin", "triple sec", "orange juice", "pineapple juice"],
+      image: "images/hawaiian-orange-blossom.jpg",
+      recipePage: 26
+    },
+    {
+      name: "Pussycat",
+      ingredients: ["gin", "grenadine", "pineapple juice"],
+      image: "images/pussycat.jpg",
+      recipePage: 31
+    },
+    {
+      name: "The Blue Train",
+      ingredients: ["gin", "triple sec", "blue curacao", "lemon juice"],
+      image: "images/blue-train.jpg",
+      recipePage: 33
+    },
+    {
+      name: "Cosmopolitan",
+      ingredients: ["vodka", "triple sec", "lime juice", "cranberry juice"],
+      image: "images/cosmopolitan.jpg",
+      recipePage: 36
+    },
+    {
+      name: "Woo-Woo",
+      ingredients: ["vodka", "peach schnapps", "cranberry juice"],
+      image: "images/woo-woo.jpg",
+      recipePage: 37
+    },
+    {
+      name: "Sex On The Beach",
+      ingredients: ["vodka", "peach schnapps", "orange juice", "cranberry juice", "lemon juice"],
+      image: "images/sex-on-the-beach.jpg",
+      recipePage: 38
+    },
+    {
+      name: "Fuzzy Navel",
+      ingredients: ["vodka", "peach schnapps", "orange juice"],
+      image: "images/fuzzy-navel.jpg",
+      recipePage: 38
+    },
+    {
+      name: "Salty Dog",
+      ingredients: ["vodka", "grapefruit juice"],
+      image: "images/salty-dog.jpg",
+      recipePage: 39
+    },
+    {
+      name: "Blue Monday",
+      ingredients: ["vodka", "cointreau", "blue curacao"],
+      image: "images/blue-monday.jpg",
+      recipePage: 43
+    },
+    {
+      name: "Mimi",
+      ingredients: ["vodka", "coconut cream", "pineapple juice"],
+      image: "images/mimi.jpg",
+      recipePage: 48
+    },
+    {
+      name: "Sea Breeze",
+      ingredients: ["vodka", "cranberry juice", "pink grapefruit juice"],
+      image: "images/sea-breeze.jpg",
+      recipePage: 50
+    },
+    {
+      name: "Screwdriver",
+      ingredients: ["vodka", "orange juice"],
+      image: "images/screwdriver.jpg",
+      recipePage: 51
+    },
+    {
+      name: "Daiquiri",
+      ingredients: ["rum", "lime juice", "sugar"],
+      image: "images/daiquiri.jpg",
+      recipePage: 56
+    },
+    {
+      name: "Piña Colada",
+      ingredients: ["rum", "pineapple juice", "coconut cream"],
+      image: "images/pina-colada.jpg",
+      recipePage: 58
+    },
+    {
+      name: "Club Mojito",
+      ingredients: ["rum", "angostura bitters", "sugar syrup", "lime", "mint", "sparkling water"],
+      image: "images/club-mojito.jpg",
+      recipePage: 58
+    },
+    {
+      name: "Ocean Breeze",
+      ingredients: ["rum", "amaretto", "blue curacao", "pineapple juice", "sparkling water"],
+      image: "images/ocean-breeze.jpg",
+      recipePage: 61
+    },
+    {
+      name: "Blue Hawaiian",
+      ingredients: ["rum", "blue curacao", "pineapple juice", "coconut cream"],
+      image: "images/blue-hawaiian.jpg",
+      recipePage: 61
+    },
+    {
+      name: "Cuba Libre",
+      ingredients: ["rum", "cola"],
+      image: "images/cuba-libre.jpg",
+      recipePage: 64
+    },
+    {
+      name: "Cuban Special",
+      ingredients: ["rum", "triple sec", "lime juice", "pineapple juice"],
+      image: "images/cuban-special.jpg",
+      recipePage: 64
+    },
+    {
+      name: "Rum Cobbler",
+      ingredients: ["rum", "sparkling water"],
+      image: "images/rum-cobbler.jpg",
+      recipePage: 65
+    },
+    {
+      name: "Frozen Peach Daiquiri",
+      ingredients: ["rum", "lime juice", "sugar syrup", "peach"],
+      image: "images/frozen-peach-daiquiri.jpg",
+      recipePage: 66
+    },
+    {
+      name: "Rum Cooler",
+      ingredients: ["rum", "pineapple juice", "lime juice", "banana"],
+      image: "images/rum-cooler.jpg",
+      recipePage: 67
+    },
+    {
+      name: "Whiskey Sour",
+      ingredients: ["whiskey", "lime juice", "sugar syrup"],
+      image: "images/whiskey-sour.jpg",
+      recipePage: 68
+    },
+    {
+      name: "Whiskey Rickey",
+      ingredients: ["whiskey", "lime juice", "sparkling water"],
+      image: "images/whiskey-rickey.jpg",
+      recipePage: 68
+    },
+    {
+      name: "Highland Fling",
+      ingredients: ["whiskey", "sweet vermouth", "angostura bitters"],
+      image: "images/highland-fling.jpg",
+      recipePage: 69
+    },
+    {
+      name: "Whiskey Sling",
+      ingredients: ["whiskey", "lemon juice"],
+      image: "images/whiskey-sling.jpg",
+      recipePage: 69
     }
   ];
